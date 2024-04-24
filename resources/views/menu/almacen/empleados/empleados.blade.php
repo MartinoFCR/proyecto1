203a1fa0b86fcfdda500f2a54025a80f055094b1
Brayan riveros <riverosb34@gmail.com>
2024-04-24 13:42:11 -0500

    empleado

---
+++ resources/views/menu/almacen/empleados/empleados.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Empleados</title>
+    <!-- <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/login.css') }}"> -->
+</head>
+
+<body>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+
+</html>
