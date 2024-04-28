ac2f09f1df56cdf51cabc96f0c80d78412bfe4a1
Martin <martin.calderon.ing@gmail.com>
2024-04-28 17:35:50 -0500

    ,

--- resources/views/menu/storages/products.blade.php
+++ resources/views/menu/storages/products.blade.php
@@ -6,6 +6,7 @@
 <title>Home</title>
 <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/reset.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/styles.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/scroll.css') }}">
 <script src="https://kit.fontawesome.com/6dd0aa23c2.js" crossorigin="anonymous"></script>
 <!--<script src="https://cdn.tailwindcss.com"></script>-->
 </head>
@@ -13,14 +14,16 @@
 
 
 <div class="layout">
-
     <div class="layout__left">
         <div class="left__title">
             <h1 class="title_menu">MENÚ</h1>
         </div>
 
         <div class="left__container">
-            <ul class="list__content">
+            <nav></nav>
+
+
+            <ul class="dropdown" id="menu">
                 <!--
                 <li>
                     
@@ -31,57 +34,115 @@
                 </li>
                 -->
 
-                <li>
-                    <a class="option__link" href="">
-                        <i class="icono"></i>
-                        <span class="option__title">ADMINISTRACIÓN</span>
+                <li class="dropdown__list">
+                    <a class="dropdown__link" href="">
+                        <i class="dropdown__icon fa-solid fa-user-tie"></i>
+                        <span class="dropdown__span">ADMINISTRACIÓN</span>
+                        <i class="dropdown__arrow fa-solid fa-chevron-down"></i>
+
+                        <input type="checkbox" class="dropdown__check">
                     </a>
-                    <ul class="list__submenu">
-                        <li><a href="/config">Ajuste de empresa</a></li>
-                        <li><a href="/categories">Categorias de prendas</a></li>
-                        <li><a href="/offices">Sucursales</a></li>
-                        <li><a href="/employees">Empleados</a></li>
-                        <li><a href="/user_profile">Perfiles de usuario</a></li>
-                        <li><a href="/users">Usuarios</a></li>
-                    </ul>
+                    
+                    <div class="dropdown__content">
+                        <ul class="dropdown__sub">
+                            <li class="dropdown__li">
+                                <a class="dropdown__anchor" href="/config">Ajuste de empresa</a>
+                            </li>
+                            <li class="dropdown__li">
+                                <a class="dropdown__anchor" href="/categories">Categorias de prendas</a>
+                            </li>
+                            <li class="dropdown__li">
+                                <a class="dropdown__anchor" href="/offices">Sucursales</a>
+                            </li>
+                            <li class="dropdown__li">
+                                <a class="dropdown__anchor" href="/employees">Empleados</a>
+                            </li>
+                            <li class="dropdown__li">
+                                <a class="dropdown__anchor" href="/user_profile">Perfiles de usuario</a>
+                            </li>
+                            <li class="dropdown__li">
+                                <a class="dropdown__anchor" href="/users">Usuarios</a>
+                            </li>
+                        </ul>
+                    </div>
                 </li>
 
-                <li>
-                    <a class="option__link" href="">
-                        <i class="icono"></i>
-                        <span class="option__title">VENTAS</span>
+                <li class="dropdown__list">
+                    <a class="dropdown__link" href="">
+                        <i class="dropdown__icon fa-solid fa-cart-shopping"></i>
+                        <span class="dropdown__span">VENTAS</span>
+                        <i class="dropdown__arrow fa-solid fa-chevron-down"></i>
+
+                        <input type="checkbox" class="dropdown__check">
                     </a>
-                    <ul class="list__submenu">
-                        <li><a href="/bills">Factura</a></li>
-                        <li><a href="/clients">Clientes</a></li>
-                        <li><a href="/quotes">Cotizacion</a></li>
-                    </ul>
+                    
+                    <div class="dropdown__content">
+                        <ul class="dropdown__sub">
+                            <li class="dropdown__li">
+                                <a class="dropdown__anchor" href="/bills">Factura</a>
+                            </li>
+                            <li class="dropdown__li">
+                                <a class="dropdown__anchor" href="/clients">Clientes</a>
+                            </li>
+                            <li class="dropdown__li">
+                                <a class="dropdown__anchor" href="/quotes">Cotizacion</a>
+                            </li>
+                        </ul>
+                    </div>
                 </li>
 
-                <li>
-                    <a class="option__link" href="">
-                        <i class="icono"></i>
-                        <span class="option__title">COMPRAS</span>
+                <li class="dropdown__list">
+                    <a class="dropdown__link" href="">
+                        <i class="dropdown__icon fa-solid fa-cart-flatbed"></i>
+                        <span class="dropdown__span">COMPRAS</span>
+                        <i class="dropdown__arrow fa-solid fa-chevron-down"></i>
+
+                        <input type="checkbox" class="dropdown__check">
                     </a>
-                    <ul class="list__submenu">
-                        <li><a href="/suppliers">Proveedores</a></li>
-                        <li><a href="/buys">Compras</a></li>
-                        <li><a href="/buys_orders">Orden de compra</a></li>
-                    </ul>
+
+                    <div class="dropdown__content">
+                        <ul class="dropdown__sub">
+                            <li class="dropdown__li">
+                                <a class="dropdown__anchor" href="/suppliers">Proveedores</a>
+                            </li>
+                            <li class="dropdown__li">
+                                <a class="dropdown__anchor" href="/buys">Compras</a>
+                            </li>
+                            <li class="dropdown__li">
+                                <a class="dropdown__anchor" href="/buys_orders">Orden de compra</a>
+                            </li>
+                        </ul>
+                    </div>
                 </li>
 
-                <li>
-                    <a class="option__link" href="">
-                        <i class="icono"></i>
-                        <span class="option__title">ALMACEN</span>
+                <li class="dropdown__list">
+                    <a class="dropdown__link" href="">
+                        <i class="dropdown__icon fa-solid fa-shop"></i>
+                        <span class="dropdown__span">ALMACEN</span>
+                        <i class="dropdown__arrow fa-solid fa-chevron-down"></i>
+
+                        <input type="checkbox" class="dropdown__check">
                     </a>
-                    <ul class="list__submenu">
-                        <li><a href="/storages">Almacenes</a></li>
-                        <li><a href="/kardex">Kardex</a></li>
-                        <li><a href="/transfers">Traslados</a></li>
-                        <li><a href="/storage_config">Ajustes</a></li>
-                        <li><a href="/products">Articulos</a></li>
-                    </ul>
+
+                    <div class="dropdown__content">
+                        <ul class="dropdown__sub">
+                            <li class="dropdown__li">
+                                <a class="dropdown__anchor" href="/storages">Almacenes</a>
+                            </li>
+                            <li class="dropdown__li">
+                                <a class="dropdown__anchor" href="/kardex">Kardex</a>
+                            </li>
+                            <li class="dropdown__li">
+                                <a class="dropdown__anchor" href="/transfers">Traslados</a>
+                            </li>
+                            <li class="dropdown__li">
+                                <a class="dropdown__anchor" href="/storage_config">Ajustes</a>
+                            </li>
+                            <li class="dropdown__li">
+                                <a class="dropdown__anchor" href="/products">Articulos</a>
+                            </li>
+                        </ul>
+                    </div>
                 </li>
             </ul>
         </div>
@@ -116,7 +177,7 @@
             <h3 class="content__info">Almacen / Articulos</h3>
 
             <div class="center-content">
-    <!-- AQUI EMPIEZA LO QUE CAMBIA EN CADA VISTA-->
+                <!-- AQUI EMPIEZA LO QUE CAMBIA EN CADA VISTA-->
 
                 <div class="content__inputs">
                     <div class="inputs__group">
@@ -132,11 +193,9 @@
 
                 </div>
 
-        <!-- AQUI TERMINA LO QUE CAMBIA EN CADA VISTA-->
+                <!-- AQUI TERMINA LO QUE CAMBIA EN CADA VISTA-->
             </div>
         </div>
-
-
     </div>
 </div>
 
@@ -145,16 +204,6 @@
         <h3 class="info__text">Pie de página</h3>
     </div>
 </div>
-<script>
-    function toggleSubMenu(option__submenuId) {
-        let option__submenu = document.getElementById(option__submenuId);
-        if (option__submenu.style.display === "block") {
-            option__submenu.style.display = "none";
-        } else {
-            option__submenu.style.display = "block";
-        }
-    }
-</script>
 
 </body>
 </html>
