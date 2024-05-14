adf2ef576c6df6c8e79c1aa20a374fbdcc43c3e0
Martin <martin.calderon.ing@gmail.com>
2024-05-13 22:44:35 -0500

    ajustes de clientes

--- resources/views/menu/sells/form_clients.blade.php
+++ resources/views/menu/sells/form_clients.blade.php
@@ -1,3 +1,6 @@
+<a href="{{ url('clients') }}">
+    <i class="return__icon fa-solid fa-left-long"></i>
+</a> 
 <h1>{{ $modo }} cliente</h1>
 @if(count($errors)>0)
 <div class="alert alert-danger" role="alert">
@@ -6,19 +9,38 @@
         <li>{{ $error }}</li>
         @endforeach
     </ul>
+
 </div>
 @endif
-<label for="id">Cedula</label>
-<input type="text" name="id" value="{{ isset($customers->id) ? $customers->id : old('id') }}" id="id">
-<label for="name_customer">Nombre</label>
-<input type="text" name="name_customer" value="{{ isset($customers->name_customer) ? $customers->name_customer : old('name_customer') }}" id="name_customer">
-<label for="lastname_customer">Apellidos</label>
-<input type="text" name="lastname_customer" value="{{ isset($customers->lastname_customer) ? $customers->lastname_customer : old('lastname_customer') }}" id="lastname_customer">
-<label for="phone">Celular</label>
-<input type="text" name="phone" value="{{ isset($customers->phone) ? $customers->phone : old('phone') }}" id="phone">
-<label for="city">Ciudad</label>
-<input type="text" name="city" value="{{ isset($customers->city) ? $customers->city : old('city') }}" id="city">
-<label for="date">Fecha de Nacimiento</label>
-<input type="date" name="date" value="{{ isset($customers->date) ? $customers->date : old('date') }}" id="date">
-<input type="submit" value="{{ $modo }} cliente">
-<a href="{{ url('clients') }}">Regresar</a>
+<br>
+<div class="form__group">
+    <input class="form__input" placeholder="Cedula" type="text" name="id" value="{{ isset($customers->id) ? $customers->id : old('id') }}" id="id">
+    <label class="form__label" for="id">Cedula</label>
+</div>
+<br>
+<div class="form__group">
+    <input class="form__input" placeholder="Nombre" type="text" name="name_customer" value="{{ isset($customers->name_customer) ? $customers->name_customer : old('name_customer') }}" id="name_customer">
+    <label class="form__label" for="name_customer">Nombre</label>
+</div>
+<br>
+<div class="form__group">
+    <input class="form__input" placeholder="Apellidos" type="text" name="lastname_customer" value="{{ isset($customers->lastname_customer) ? $customers->lastname_customer : old('lastname_customer') }}" id="lastname_customer">
+    <label class="form__label" for="lastname_customer">Apellidos</label>
+</div>
+<br>
+<div class="form__group">
+    <input class="form__input" placeholder="Celular" type="text" name="phone" value="{{ isset($customers->phone) ? $customers->phone : old('phone') }}" id="phone">
+    <label class="form__label" for="phone">Celular</label>
+</div>
+<br>
+<div class="form__group">
+    <input class="form__input" placeholder="Ciudad" type="text" name="city" value="{{ isset($customers->city) ? $customers->city : old('city') }}" id="city">
+    <label class="form__label" for="city">Ciudad</label>
+</div>
+<br>
+<div class="form__group">
+    <input class="form__input" placeholder="Fecha de Nacimiento" type="date" name="date" value="{{ isset($customers->date) ? $customers->date : old('date') }}" id="date">
+    <label class="form__label" for="date">Fecha de Nacimiento</label>
+</div>
+<br>
+    <input class="form__buttom" type="submit" value="{{ $modo }} cliente">
