23a203ca471bf66ad6ce78544a6f85ea7e44dbc4
Martin <martin.calderon.ing@gmail.com>
2024-05-16 22:38:30 -0500

    ajustes graficos home

--- resources/views/menu/home.blade.php
+++ resources/views/menu/home.blade.php
@@ -7,6 +7,9 @@
 <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/reset.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/styles.css') }}">
 <script src="https://kit.fontawesome.com/6dd0aa23c2.js" crossorigin="anonymous"></script>
+<script src="https://code.highcharts.com/highcharts.js"></script>
+<script src="https://code.highcharts.com/modules/exporting.js"></script>
+<script src="https://code.highcharts.com/modules/accessibility.js"></script>
 <!--<script src="https://cdn.tailwindcss.com"></script>-->
 </head>
 <body>
@@ -167,92 +170,179 @@
 
             <h3 class="content__info">Bienvenido</h3>
 
-            <div class="center-content">
-                <script src="https://code.highcharts.com/highcharts.js"></script>
-                <script src="https://code.highcharts.com/modules/exporting.js"></script>
-                <script src="https://code.highcharts.com/modules/accessibility.js"></script>
+            <div class="center-content home">
+                <div class="figures">
+                    <figure class="highcharts-figure">
+                        <div id="container">
+                            <script>
+                                Highcharts.chart('container', {
+                                chart: {
+                                    type: 'pie'
+                                },
+                                title: {
+                                    text: 'Egg Yolk Composition'
+                                },
+                                tooltip: {
+                                    valueSuffix: '%'
+                                },
+                                subtitle: {
+                                    text:
+                                    'Source:<a href="https://www.mdpi.com/2072-6643/11/3/684/htm" target="_default">MDPI</a>'
+                                },
+                                plotOptions: {
+                                    series: {
+                                        allowPointSelect: true,
+                                        cursor: 'pointer',
+                                        dataLabels: [{
+                                            enabled: true,
+                                            distance: 20
+                                        }, {
+                                            enabled: true,
+                                            distance: -40,
+                                            format: '{point.percentage:.1f}%',
+                                            style: {
+                                                fontSize: '1.2em',
+                                                textOutline: 'none',
+                                                opacity: 0.7
+                                            },
+                                            filter: {
+                                                operator: '>',
+                                                property: 'percentage',
+                                                value: 10
+                                            }
+                                        }]
+                                    }
+                                },
+                                series: [
+                                    {
+                                        name: 'Percentage',
+                                        colorByPoint: true,
+                                        data: [
+                                            {
+                                                name: 'Water',
+                                                y: 55.02
+                                            },
+                                            {
+                                                name: 'Fat',
+                                                sliced: true,
+                                                selected: true,
+                                                y: 26.71
+                                            },
+                                            {
+                                                name: 'Carbohydrates',
+                                                y: 1.09
+                                            },
+                                            {
+                                                name: 'Protein',
+                                                y: 15.5
+                                            },
+                                            {
+                                                name: 'Ash',
+                                                y: 1.68
+                                            }
+                                        ]
+                                    }
+                                ]
+                                });
+                            </script>
+                        </div>
+                    </figure>
 
-                <figure class="highcharts-figure">
-                    <div id="container">
-                        <script>
-                            Highcharts.chart('container', {
-    chart: {
-        type: 'pie'
-    },
-    title: {
-        text: 'Egg Yolk Composition'
-    },
-    tooltip: {
-        valueSuffix: '%'
-    },
-    subtitle: {
-        text:
-        'Source:<a href="https://www.mdpi.com/2072-6643/11/3/684/htm" target="_default">MDPI</a>'
-    },
-    plotOptions: {
-        series: {
-            allowPointSelect: true,
-            cursor: 'pointer',
-            dataLabels: [{
-                enabled: true,
-                distance: 20
-            }, {
-                enabled: true,
-                distance: -40,
-                format: '{point.percentage:.1f}%',
-                style: {
-                    fontSize: '1.2em',
-                    textOutline: 'none',
-                    opacity: 0.7
-                },
-                filter: {
-                    operator: '>',
-                    property: 'percentage',
-                    value: 10
-                }
-            }]
-        }
-    },
-    series: [
-        {
-            name: 'Percentage',
-            colorByPoint: true,
-            data: [
-                {
-                    name: 'Water',
-                    y: 55.02
-                },
-                {
-                    name: 'Fat',
-                    sliced: true,
-                    selected: true,
-                    y: 26.71
-                },
-                {
-                    name: 'Carbohydrates',
-                    y: 1.09
-                },
-                {
-                    name: 'Protein',
-                    y: 15.5
-                },
-                {
-                    name: 'Ash',
-                    y: 1.68
-                }
-            ]
-        }
-    ]
-});
+                    <figure class="highcharts-figure">
+                        <div id="container2">
+                            <script>
+                                Highcharts.chart('container2', {
 
-                        </script>
-                    </div>
-                    <p class="highcharts-description">
-                        Pie charts are very popular for showing a compact overview of a
-                        composition or comparison. While they can be harder to read than
-                        column charts, they remain a popular choice for small datasets.
-                    </p>
-                </figure>
+                                title: {
+                                    text: 'U.S Solar Employment Growth',
+                                    align: 'left'
+                                },
+
+                                subtitle: {
+                                    text: 'By Job Category. Source: <a href="https://irecusa.org/programs/solar-jobs-census/" target="_blank">IREC</a>.',
+                                    align: 'left'
+                                },
+
+                                yAxis: {
+                                    title: {
+                                        text: 'Number of Employees'
+                                    }
+                                },
+
+                                xAxis: {
+                                    accessibility: {
+                                        rangeDescription: 'Range: 2010 to 2020'
+                                    }
+                                },
+
+                                legend: {
+                                    layout: 'vertical',
+                                    align: 'right',
+                                    verticalAlign: 'middle'
+                                },
+
+                                plotOptions: {
+                                    series: {
+                                        label: {
+                                            connectorAllowed: false
+                                        },
+                                        pointStart: 2010
+                                    }
+                                },
+
+                                series: [{
+                                    name: 'Installation & Developers',
+                                    data: [
+                                        43934, 48656, 65165, 81827, 112143, 142383,
+                                        171533, 165174, 155157, 161454, 154610
+                                    ]
+                                }, {
+                                    name: 'Manufacturing',
+                                    data: [
+                                        24916, 37941, 29742, 29851, 32490, 30282,
+                                        38121, 36885, 33726, 34243, 31050
+                                    ]
+                                }, {
+                                    name: 'Sales & Distribution',
+                                    data: [
+                                        11744, 30000, 16005, 19771, 20185, 24377,
+                                        32147, 30912, 29243, 29213, 25663
+                                    ]
+                                }, {
+                                    name: 'Operations & Maintenance',
+                                    data: [
+                                        null, null, null, null, null, null, null,
+                                        null, 11164, 11218, 10077
+                                    ]
+                                }, {
+                                    name: 'Other',
+                                    data: [
+                                        21908, 5548, 8105, 11248, 8989, 11816, 18274,
+                                        17300, 13053, 11906, 10073
+                                    ]
+                                }],
+
+                                responsive: {
+                                    rules: [{
+                                        condition: {
+                                            maxWidth: 500
+                                        },
+                                        chartOptions: {
+                                            legend: {
+                                                layout: 'horizontal',
+                                                align: 'center',
+                                                verticalAlign: 'bottom'
+                                            }
+                                        }
+                                    }]
+                                }
+
+                                });
+
+                            </script>
+                        </div>
+                    </figure>
+                </div>
             </div>
         </div>
 
